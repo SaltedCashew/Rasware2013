e83af6192026359218c6b73ffdfb61616b8580c1
Ahmed Abdellatif <a.abdellatif10@gmail.com>
2013-10-11 20:38:32 -0500

    BlinkyLED Up and Running

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,14 +1,17 @@
+#include <RASLib/inc/gpio.h>
+#include <RASLib/inc/time.h>
 #include <RASLib/inc/common.h>
 
 // The 'main' function is the entry point of the program
+void delay(int);
+void SetPin(tPin, tBoolean);
 int main(void) {
-    InitializeMCU();
-    
-    // Initialization code can go here
-    
-    while (1) {
-        
-        // Runtime code can go here
-        
-    }
+	InitializeMCU();
+	
+  while (1) {
+		SetPin(PIN_F3, true);
+		Wait(0.5);
+		SetPin(PIN_F3, false);
+		Wait(0.5);
+	}
 }
