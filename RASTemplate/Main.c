60f8404eceafcb003b41f8313c80b7b7992d0409
Ahmed Abdellatif <a.abdellatif10@gmail.com>
2013-10-18 19:08:12 -0500

    Straight Line Checkpoint

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,17 +1,18 @@
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
+#include <RASLib/inc/motor.h>
 #include <RASLib/inc/common.h>
 
 // The 'main' function is the entry point of the program
 void delay(int);
 void SetPin(tPin, tBoolean);
 int main(void) {
+	tMotor *motor[2];
 	InitializeMCU();
-	
-  while (1) {
-		SetPin(PIN_F3, true);
-		Wait(0.5);
-		SetPin(PIN_F3, false);
-		Wait(0.5);
+	motor[0] = InitializeMotor(PIN_F3, PIN_F2, true, false);
+	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, false);
+  while (1) {		
+		SetMotor(motor[0], -0.25);
+		SetMotor(motor[1], -0.25);
 	}
 }
