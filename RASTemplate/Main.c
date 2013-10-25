51a2e181ecfa0c1bd0cec28da4ca46595b5b2f89
Ahmed Abdellatif <a.abdellatif10@gmail.com>
2013-10-25 17:47:11 -0500

    Line Sensor - INCOMPLETE

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -1,18 +1,40 @@
 #include <RASLib/inc/gpio.h>
 #include <RASLib/inc/time.h>
 #include <RASLib/inc/motor.h>
+#include <RASLib/inc/linesensor.h>
+#include <RASLib/inc/i2c.h>
 #include <RASLib/inc/common.h>
-
 // The 'main' function is the entry point of the program
 void delay(int);
 void SetPin(tPin, tBoolean);
+tLineSensor *ls;
 int main(void) {
-	tMotor *motor[2];
+	unsigned char read;
+	tI2C *bus;
+//	tMotor *motor[2];
 	InitializeMCU();
-	motor[0] = InitializeMotor(PIN_F3, PIN_F2, true, false);
-	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, false);
-  while (1) {		
-		SetMotor(motor[0], -0.25);
-		SetMotor(motor[1], -0.25);
+	bus = InitializeI2C(PIN_B3, PIN_B2);
+	ls = InitializeLineSensor(bus, 0);
+/*	motor[0] = InitializeMotor(PIN_C5, PIN_C4, true, false); //right wheel
+	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, false); //left wheel
+	SetMotor(motor[0], -0.20);
+	SetMotor(motor[1], -0.20); */
+	while(1){
+		read = LineSensorRead(ls, 0.75);
+		Printf("%x ", read);
+		Wait(1);
+/*		if (read >= 1 && read <= 40){
+			SetMotor(motor[0], -0.30); //turn left
+			SetMotor(motor[1], -0.10);
+		}
+		else if (read >= 150){
+			SetMotor(motor[1], -0.20); //turn right
+			SetMotor(motor[0], -0.10);
+		}
+		else{
+			SetMotor(motor[0], -0.20);
+			SetMotor(motor[1], -0.20);
+		}
+		*/
 	}
 }
