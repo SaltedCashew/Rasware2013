a53b6c0a69da0eff7032dfc2aa65f8e995e9ef20
Brad <a10gac@gmail.com>
2013-10-25 18:42:52 -0500

    Working Line Following Code

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -11,30 +11,30 @@ tLineSensor *ls;
 int main(void) {
 	unsigned char read;
 	tI2C *bus;
-//	tMotor *motor[2];
+	tMotor *motor[2];
 	InitializeMCU();
 	bus = InitializeI2C(PIN_B3, PIN_B2);
 	ls = InitializeLineSensor(bus, 0);
-/*	motor[0] = InitializeMotor(PIN_C5, PIN_C4, true, false); //right wheel
+	motor[0] = InitializeMotor(PIN_C5, PIN_C4, true, false); //right wheel
 	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, false); //left wheel
-	SetMotor(motor[0], -0.20);
-	SetMotor(motor[1], -0.20); */
+	SetMotor(motor[0], -0.30); //Good at -0.20
+	SetMotor(motor[1], -0.30); //Good at -0.20
 	while(1){
 		read = LineSensorRead(ls, 0.75);
-		Printf("%x ", read);
-		Wait(1);
-/*		if (read >= 1 && read <= 40){
-			SetMotor(motor[0], -0.30); //turn left
-			SetMotor(motor[1], -0.10);
+	//	Printf("%x ", read);
+	//	Wait(1);
+		if (read >= 1 && read <= 40){
+			SetMotor(motor[0], -0.40); //turn left - tested at -0.30
+			SetMotor(motor[1], 0.00);
 		}
 		else if (read >= 150){
-			SetMotor(motor[1], -0.20); //turn right
-			SetMotor(motor[0], -0.10);
+			SetMotor(motor[1], -0.40); //turn right - tested at -0.30
+			SetMotor(motor[0], 0.00);
 		}
 		else{
-			SetMotor(motor[0], -0.20);
-			SetMotor(motor[1], -0.20);
+			SetMotor(motor[0], -0.30);
+			SetMotor(motor[1], -0.30);
 		}
-		*/
+		
 	}
 }
