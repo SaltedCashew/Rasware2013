6a85d30bd7575b166da4df83dbdbc134e9229162
Brad <a10gac@gmail.com>
2013-11-02 17:55:47 -0500

    Cleaned up code for Wall Following...

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -5,105 +5,67 @@
 #include <RASLib/inc/i2c.h>
 #include <RASLib/inc/common.h>
 #include <RASLib/inc/adc.h>
-// The 'main' function is the entry point of the program
+
 
 void delay(int);
 void SetPin(tPin, tBoolean);
-tLineSensor *ls;
+
 
 
 
 int main(void) {
-//	unsigned char read;
-	tI2C *bus;
+
 	tADC *adc[4];
 	tMotor *motor[2];
 	float ADCValue;
+	float lmSpeed;
+	float rmSPeed;
 
 	InitializeMCU();
-	bus = InitializeI2C(PIN_B3, PIN_B2);
-	ls = InitializeLineSensor(bus, 0);
-	motor[0] = InitializeMotor(PIN_F2, PIN_F3, true, false); //right wheel
-	motor[1] = InitializeMotor(PIN_F0, PIN_F1, true, true); //left wheel
-	adc[0] = InitializeADC(PIN_E5);
 
+	motor[0] = InitializeMotor(PIN_F1, PIN_F0, true, true); //left wheel
+	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, true); //right wheel
+	adc[0] = InitializeADC(PIN_D0);
 
-	SetMotor (motor[1], -.15);
-	SetMotor (motor[0], -.150);
+lmSpeed = .04
+rmSpeed = .03
+	SetMotor(motor[1], lmSpeed); 
+			SetMotor(motor[0], smSpeed);
+
 	
 	while(1){
-
+Printf("start\n");
 		ADCValue = ADCRead(adc[0]);
-	//	ADCValue *= 1000; 
-	SetMotor (motor[0], -.15 * ADCValue); //change the right wheel only
+		
+	// SetMotor (motor[0], 1 - (ADCValue)); //change the right wheel only
   
-	Printf("IR1 values: %d\t", (int)(1000 * ADCValue));	
-		Wait(.01);
-		
-		
+	// Printf("IR1 values: %d\t", (ADCValue));	
+ //  Printf(" %d\n", (int)(ADCValue*1000));
 
-		
-			
-	/*	if (ADCValue <= 600) {
-			SetMotor(motor[0], -0.35);  
-			SetMotor(motor[1], -0.25);
+	
+	
+	
+		if (ADCValue >= 800){
+			SetMotor(motor[1], rmSpeed -0.1); //turn right
+			SetMotor(motor[0], lmSpeed +0.1);
 		}
 		
-		else  {
-			SetMotor(motor[0], -.25); 
-			SetMotor(motor[1], -0.35);
-		}
-*/			
-		
-		
-		/*
-		if (ADCValue >= 700){
-			SetMotor(motor[0], -0.15); //turn right
-			SetMotor(motor[1], -.25);
+		if (ADCValue <= 	100) {
+			SetMotor(motor[1], rmSpeed+.1); //turn Left 
+			SetMotor(motor[0], lmSpeed-.1);
 		}
 		
-		if (ADCValue <= 600 &&  ADCValue>400) {
-			SetMotor(motor[0], -0.25); //turn Left 
-			SetMotor(motor[1], -.20);
-		}
 		
-		else if (ADCValue < 400) {
-			SetMotor(motor[0], -0.30); //turn Left Faster
-			SetMotor(motor[1], -0.00);
-		}
 		
 		else { 
-			SetMotor(motor[1], -0.25); 
-			SetMotor(motor[0], -0.25);
+			SetMotor(motor[1], rmSpeed); 
+			SetMotor(motor[0], lmSpeed);
+			
+
+		Wait(.01);
 		}
-		*/
-		/*		read = LineSensorRead(ls, 0.75);
-	//	Printf("%x ", read);
-	//	Wait(1);
-		if (read >= 1 && read <= 40){
-			SetMotor(motor[0], -0.40); //turn left - tested at -0.30
-			SetMotor(motor[1], 0.00);
-		}
-		else if (read >= 150 && read < 255){ //tested at 90
-			SetMotor(motor[1], -0.40); //turn right - tested at -0.30
-			SetMotor(motor[0], 0.00);
-		}
-//slow down if no line	
-		else if (read == 0) {
-			SetMotor(motor[0], -0.15);
-			SetMotor(motor[1], -0.15);
-		} */
-	/* 255 = all black line	
-		else if (read == 255) {
-			SetMotor(motor[0], 0.0);
-			SetMotor(motor[1], 0.0);
-		} 
-	*/	
-	/*	else{
-			SetMotor(motor[0], -0.30);
-			SetMotor(motor[1], -0.30);
-		}
-		*/
+		
+	
 	}
 }
 
