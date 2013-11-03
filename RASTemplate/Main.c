521bb66b277a5d4f17861dddf9f41e3739032d30
bdehoyos <bdehoyos@utexas.edu>
2013-11-03 00:57:19 -0500

    Line Following code - Passing checkpoint

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -15,55 +15,66 @@ void SetPin(tPin, tBoolean);
 
 int main(void) {
 
-	tADC *adc[4];
+	tADC *adc[1];
 	tMotor *motor[2];
 	float ADCValue;
-	float lmSpeed;
-	float rmSPeed;
-
+	double lmSpeed;
+	double rmSpeed;
+	
 	InitializeMCU();
 
-	motor[0] = InitializeMotor(PIN_F1, PIN_F0, true, true); //left wheel
-	motor[1] = InitializeMotor(PIN_B7, PIN_B6, true, true); //right wheel
+motor[0] = InitializeMotor(PIN_B7, PIN_B6, true, false); //right motor
+	motor[1] = InitializeMotor(PIN_C5, PIN_C4, true, true);
+	
+	//zero values
+	//		SetMotor(motor[0], -.52);
+	//  	SetMotor(motor[1], -.75); //-0.5 is stopped --
 	adc[0] = InitializeADC(PIN_D0);
 
-lmSpeed = .04
-rmSpeed = .03
-	SetMotor(motor[1], lmSpeed); 
-			SetMotor(motor[0], smSpeed);
+	
+	
+	lmSpeed = -.2;
+	rmSpeed = -.3;
+	
+		//SetMotor(motor[0], rmSpeed);
+		SetMotor(motor[1], lmSpeed);
 
 	
 	while(1){
 Printf("start\n");
 		ADCValue = ADCRead(adc[0]);
 		
-	// SetMotor (motor[0], 1 - (ADCValue)); //change the right wheel only
+	
   
 	// Printf("IR1 values: %d\t", (ADCValue));	
- //  Printf(" %d\n", (int)(ADCValue*1000));
-
+  //Printf(" %d\n", (int)(ADCValue*1000));
+  // ADCValue *= 1000;
+	SetMotor (motor[0], -1 + (ADCValue)); //change the right wheel only --
+		// close distance = high value
 	
 	
-	
-		if (ADCValue >= 800){
-			SetMotor(motor[1], rmSpeed -0.1); //turn right
-			SetMotor(motor[0], lmSpeed +0.1);
+	/*	if (ADCValue >= 650){
+			SetMotor(motor[0], rmSpeed +.2); //turn right
+			
 		}
 		
-		if (ADCValue <= 	100) {
-			SetMotor(motor[1], rmSpeed+.1); //turn Left 
-			SetMotor(motor[0], lmSpeed-.1);
-		}
+	//	else if (ADCValue <500 && ADCValue<800)
+	//	{ SetMotor(motor[0], rmSpeed - 0.05) ;
+	//	}
 		
+		else if (ADCValue < 	600) {
+			
+			SetMotor(motor[0], rmSpeed -.15); //turn left
+		}
 		
 		
 		else { 
-			SetMotor(motor[1], rmSpeed); 
-			SetMotor(motor[0], lmSpeed);
-			
-
-		Wait(.01);
+			SetMotor(motor[1], lmSpeed); 
+			SetMotor(motor[0], rmSpeed);
 		}
+*/
+		Wait(.01);
+		
 		
 	
 	}
