c3563cedb4c1d6f46a4e2c5cb6a3dd2140293635
Ahmed Abdellatif <a.abdellatif10@gmail.com>
2013-11-15 22:13:12 -0600

    Encoder WORKING (or winning at least)

--- RASTemplate/Main.c
+++ RASTemplate/Main.c
@@ -18,20 +18,38 @@ int main(void) {
 
 	tMotor *motor[2];
 	tEncoder *encoder[2];
+	int waiting = 0;
 	double lmSpeed;
 	double rmSpeed;
 	InitializeMCU();
 	motor[0] = InitializeMotor(PIN_B7, PIN_B6, true, true); //left
 	motor[1] = InitializeMotor(PIN_C5, PIN_C4, true, true); //right
-	encoder[0] = InitializeEncoder(PIN_B5, PIN_D1, false);
+	rmSpeed = -.48;
+	lmSpeed = -.48;
+	SetMotor(motor[0], lmSpeed);
+	SetMotor(motor[1], rmSpeed);
+//	encoder[0] = InitializeEncoder(PIN_A3, PIN_E1, false);
 	encoder[1] = InitializeEncoder(PIN_E3, PIN_A7, false);
-	rmSpeed = -.4;
-	lmSpeed = -.3;
-//	SetMotor(motor[0], lmSpeed);
-//	SetMotor(motor[1], rmSpeed);
 	while(1){
-		Printf("Encoder 0: %d   Encoder 1: %d\n", GetEncoder(encoder[0]), GetEncoder(encoder[1]));
-		Wait(1);
+//		Printf("Encoder 1: %d\n", GetEncoder(encoder[1]));
+/*		while(GetEncoder(encoder[1]) < (7800 * 11)){
+			SetMotor(motor[0], 0);
+			SetMotor(motor[1], rmSpeed);
+			Wait(0.1);
+			SetMotor(motor[0], lmSpeed);
+			SetMotor(motor[1], 0);
+			Wait(0.105);
+		}
+		*/
+		if (GetEncoder(encoder[1]) >= ((7500 + waiting) * 11)){
+			SetMotor(motor[0], lmSpeed);
+			SetMotor(motor[1], 0);
+			Wait(0.6);
+			ResetEncoder(encoder[1]);
+			waiting += 500;
+		}
+		SetMotor(motor[0], lmSpeed);
+		SetMotor(motor[1], rmSpeed);
 	}
 }
 
